a1f62e832f6bbec44e7d2950b2e54ca297740d3a
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-07-27 05:15:54 +0300

    Updated Dropdown component.

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -7,7 +7,6 @@
       <div class="dropdown-item-content">
         <slot name="content"></slot>
       </div>
-      <div class="dropdown-item-tail"></div>
     </div>
   </div>
 </template>
@@ -43,14 +42,14 @@ export default {
     display: inline-block;
   }
   &-item {
-    opacity: 0;
+    display: none;
     pointer-events: none;
     z-index: 10;
     transition: opacity 0.25s ease;
     position: relative;
 
     &-clicked {
-      opacity: 1;
+      display: block;
       pointer-events: auto;
     }
 
@@ -59,19 +58,6 @@ export default {
       box-shadow: 0 0 0.75rem 0.25rem rgba(0, 0, 0, 0.2);
       max-width: 280px;
       border-radius: 0.5rem;
-    }
-
-    &-tail {
-      z-index: 11;
-      position: absolute;
-      top: -3.5px;
-      left: 2rem;
-      width: 0.5rem;
-      height: 0.5rem;
-      transform: rotate(135deg);
-      background-color: #ffffff;
-      box-shadow: 0 12px 28px 0 rgba(0, 0, 0, 0.2),
-      0 2px 4px 0 rgba(0, 0, 0, 0.1), inset 0 0 0 1px rgba(255, 255, 255, 0.5);
     }
   }
 }
