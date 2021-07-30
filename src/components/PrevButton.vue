8fe4879af20fe0ef623fd7597f589467afc3a3cf
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-07-30 16:17:20 +0300

    Updated prev button component.

--- src/components/PrevButton.vue
+++ src/components/PrevButton.vue
@@ -31,7 +31,6 @@ export default {
   border-radius: 50%;
   box-shadow: 0 12px 28px 0 rgba(0, 0, 0, 0.2), 0 2px 4px 0 rgba(0, 0, 0, 0.1),
     inset 0 0 0 1px rgba(255, 255, 255, 0.5);
-  margin-left: -2.875rem;
   cursor: pointer;
   transform: scale(1);
   transition: background-color, transform 0.25s ease;
