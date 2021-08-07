c94ebcd08ffa881001ce11245a73c8251f3a806b
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-08-07 16:35:33 +0300

    Components(AutocompleteInput): updation component.

--- src/components/AutocompleteInput.vue
+++ src/components/AutocompleteInput.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="Autocomplete-Input">
-    <div class="Autocomplete-Input-pc">
-      <TextInput v-model="value" :label="label" type="text" @input="onInput" />
+    <div class="Autocomplete-Input-pc" v-on:keyup.down="onKeyDown" v-on:keyup.up="onKeyUp">
+      <TextInput ref="TextInput" v-model="value" :label="label" type="text" @input="onInput"
+                  />
 
       <div v-if="isActive" class="Autocomplete-Input-dropdown">
-        <div v-for="(item, index) in items" :key="index" class="Autocomplete-Input-dropdown-item">
-          {{ item }}
+        <div v-for="(item, index) in resultItems" :key="index" :tabindex="index + 1"
+             class="Autocomplete-Input-dropdown-item" @click="onClickToItem" v-html="item">
         </div>
       </div>
     </div>
@@ -23,14 +24,15 @@ export default {
       required: true
     },
     items: {
-      type: Object,
+      type: Array,
       required: true
     }
   },
   data() {
     return {
       value: "",
-      isActive: false
+      isActive: false,
+      resultItems: [],
     };
   },
   components: {
@@ -38,7 +40,15 @@ export default {
   },
   methods: {
     onInput() {
-      this.isActive = Boolean(this.value);
+      this.isActive = Boolean(this.value) &&
+        (this.resultItems = this.items.filter(item => item.includes(this.value))) &&
+        (this.resultItems = this.resultItems.map(item => {
+          return item.replace(this.value, `<b>${this.value}</b>`)
+        }));
+    },
+    onClickToItem(event) {
+      this.$refs.TextInput.content = event.target.innerText;
+      this.isActive = false;
     }
   }
 };
@@ -48,9 +58,11 @@ export default {
 @import "../assets/styles/global";
 
 .Autocomplete-Input {
+  position: relative;
+
   &-pc {
     .Autocomplete-Input-dropdown {
-      position: relative;
+      position: absolute;
       width: 100%;
       display: flex;
       flex-direction: column;
@@ -58,12 +70,19 @@ export default {
       overflow: hidden;
       box-shadow: 0 0 28px 0 rgba(0, 0, 0, 0.2), 0 0 4px 0 rgba(0, 0, 0, 0.1),
       inset 0 0 0 1px rgba(255, 255, 255, 0.5);
+      z-index: 10;
+      background-color: $color-white;
 
       &-item {
         padding: 0.5rem 0.5rem;
         cursor: pointer;
+        outline: none;
 
         &:hover {
+          background-color: $color-platinum;
+        }
+
+        &:focus {
           background-color: $color-platinum;
         }
       }
