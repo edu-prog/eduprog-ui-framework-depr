72c0389e79e829a3724ce68bf86323666658db83
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-09-01 17:23:13 +0300

    Feature(Collapse): Collapse menu component was created

---
+++ src/components/Collapse.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="collapse">
+    <div class="collapse-target" @click="$emit('click'); isActive = !isActive">
+      <slot name="target"/>
+    </div>
+
+    <transition appear name="fade">
+      <div v-show="isActive" class="collapse-content">
+        <slot name="content"></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Collapse",
+  data() {
+    return {
+      isActive: false,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.collapse {
+  &-target {
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  &-content {
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s linear;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
