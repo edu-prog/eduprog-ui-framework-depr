68dd0d5ba54e2d9bdacc0efd887ffa1b4903281c
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-10-31 05:05:57 +0300

    feat(Grid.vue): updated the component

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,9 +1,7 @@
-<script>
+<script lang="ts">
 export { default as Row } from "./Row.vue";
 export { default as Column } from "./Column.vue";
 export { default as Container } from "./Container.vue";
-
-export default {};
 </script>
 
 <style lang="scss">
