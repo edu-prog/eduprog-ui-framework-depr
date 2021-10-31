65076d8ff19755f26eb0b3dcd1ae46d2d82771cf
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-10-31 17:44:34 +0300

    feat(AutocompleteInput. vue): deleted unnecessary style-code

--- src/components/AutocompleteInput.vue
+++ src/components/AutocompleteInput.vue
@@ -323,17 +323,6 @@ export default defineComponent({
   opacity: 0;
 }
 
-.pop-enter-active,
-.pop-leave-active {
-  transition: transform 0.4s cubic-bezier(0.5, 0, 0.5, 1), opacity 0.4s linear;
-}
-
-.pop-enter-from,
-.pop-leave-to {
-  opacity: 0;
-  transform: scale(0.3) translateY(-50%);
-}
-
 .slide-enter-from,
 .slide-leave-from {
   transform: translateY(100%);
