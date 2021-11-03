c53ca32fef1f5f538081c55d15a191416e921efa
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-11-03 04:22:23 +0300

    chore: updated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import Button from "./components/Button.vue";
+import Row from "components/Row.vue";
+import Container from "components/Container.vue";
+import Column from "components/Column.vue";
 
-export { Button };
+export { Button, Row, Container, Column };
