10ca894176310ccb01de3ec70b95412fc051f231
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-11-03 04:58:35 +0300

    chore: updated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,59 @@
+import AutocompleteInput from "./components/AutocompleteInput.vue";
+import Avatar from "./components/Avatar.vue";
+import Badge from "./components/Badge.vue";
 import Button from "./components/Button.vue";
-import Row from "./components/Row.vue";
-import Container from "./components/Container.vue";
+import Card from "./components/Card.vue";
+import Checkbox from "./components/Checkbox.vue";
+import CheckboxBlock from "./components/CheckboxBlock.vue";
+import CheckboxBlockButton from "./components/CheckboxBlockButton.vue";
+import Collapse from "./components/Collapse.vue";
 import Column from "./components/Column.vue";
+import Container from "./components/Container.vue";
+import Dropdown from "./components/Dropdown.vue";
+import Heading from "./components/Heading.vue";
+import Icon from "./components/Icon.vue";
+import IconButton from "./components/IconButton.vue";
+import Link from "./components/Link.vue";
+import Modal from "./components/Modal.vue";
+import MultipleSelect from "./components/MultipleSelect.vue";
+import Pagination from "./components/Pagination.vue";
+import Paragraph from "./components/Paragraph.vue";
+import PhoneInput from "./components/PhoneInput.vue";
+import PrevButton from "./components/PrevButton.vue";
+import Progress from "./components/Progress.vue";
+import Row from "./components/Row.vue";
+import Select from "./components/Select.vue";
+import Spinner from "./components/Spinner.vue";
+import TextInput from "./components/TextInput.vue";
+import TextInputButton from "./components/TextInputButton.vue";
 
-export { Button, Row, Container, Column };
+export {
+  AutocompleteInput,
+  Avatar,
+  Badge,
+  Button,
+  Card,
+  Checkbox,
+  CheckboxBlock,
+  CheckboxBlockButton,
+  Collapse,
+  Column,
+  Container,
+  Dropdown,
+  Heading,
+  Icon,
+  IconButton,
+  Link,
+  Modal,
+  MultipleSelect,
+  Pagination,
+  Paragraph,
+  PhoneInput,
+  PrevButton,
+  Progress,
+  Row,
+  Select,
+  TextInput,
+  Spinner,
+  TextInputButton,
+};
