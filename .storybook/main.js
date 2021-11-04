1bdc4d6847736b5f4be0aa02ea2ee5e489f56a95
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-11-04 03:04:58 +0300

    chore: updated of packages

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    {
+      name: "@storybook/preset-scss",
+      options: {
+        cssLoaderOptions: {
+          modules: true,
+          localIdentName: "[name]__[local]--[hash:base64:5]",
+        },
+      },
+    },
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-typescript",
+  ],
+};
